a739c4093037384c8edfbd3067c5fdeb13ba10d3
harrison-lt <harryscout@gmail.com>
2025-05-25 18:09:46 -0700

    refactor(vector-lib): standardize fununction format for clarity

--- lib/vector.c
+++ lib/vector.c
@@ -3,7 +3,6 @@
 #include <stdint.h>
 #include <string.h>
 #include <stdio.h>
-#include <time.h>
 #include <assert.h>
 
 
@@ -14,6 +13,7 @@ typedef struct{
 	size_t reserve;
 	size_t data_size;
 }Vector;
+
 
 Vector vector_create(size_t data_size, size_t reserve_pow_2) 
 {
@@ -30,10 +30,10 @@ Vector vector_create(size_t data_size, size_t reserve_pow_2)
 	return vec;
 }
 
-//need to also add an emplace function for faster pushing
+
 int vector_push_back(Vector *vec, const void *item)
 {
-
+	// check if vector needs to grow
 	if (vec->length >= vec->capacity) 
 	{
 		vec->capacity *= 2;
@@ -42,44 +42,49 @@ int vector_push_back(Vector *vec, const void *item)
 		vec->array = tmp;
 	}
 	
-	 void *slot  = (vec->array) + (vec->length * vec->data_size);
-	 memcpy(slot, item, vec->data_size);
+	void *empty_slot  = (vec->array) + (vec->length * vec->data_size);
+	memcpy(empty_slot, item, vec->data_size);
 	 
 	vec->length++;
 	return 0;
 }
 
+
 void *vector_emplace_back(Vector *vec) 
 {
+	// check if vector needs to grow
 	if (vec->length >= vec->capacity) 
 	{
 		vec->capacity *= 2;
 		void *tmp = realloc(vec->array, (vec->capacity * vec->data_size));
-		if (!tmp) return NULL; // allocation failed
+		if (!tmp) 
+			return NULL; // allocation failed
 		vec->array = tmp;
 	}
 
-	void *slot = vec->array + (vec->length * vec->data_size);
+	void *empty_slot = vec->array + (vec->length * vec->data_size);
+
 	vec->length++;
-	return slot;
+	return empty_slot;
 }
+
 
 inline void* vector_get_idx(Vector *vec, size_t index)
 {
-	if (index >= vec->length) {
-		return NULL; // access out of bounds 
-	}
+	if (index >= vec->length) 
+		return NULL; // out of bounds 
 
 	return vec->array + (index * vec->data_size);
 }
 
-inline void* vector_unsafe_get_idx(Vector *vec, size_t index)
+
+static inline void* vector_unsafe_get_idx(Vector *vec, size_t index)
 {
-	void *slot = vec->array + (index * vec->data_size);
-	return slot;
+	return vec->array + (index * vec->data_size);
 }
 
-ssize_t vector_indexof(Vector *vec, void *item) 
+
+ssize_t vector_idx_of(Vector *vec, void *item) 
 {
 	uint8_t *base = vec->array;
 	uint8_t *ptr = item;
@@ -89,59 +94,92 @@ ssize_t vector_indexof(Vector *vec, void *item)
 	if(ptr < base || ptr >= (base + (total_bytes))) 
 		return -1; // out of bounds
 	
-	size_t offset = ptr - base;
+	size_t offset = (ptr - base);
 
 	if(offset % vec->data_size) 
 		return -1; // data not aligned
-	return offset / vec->data_size;
+	return (offset / vec->data_size);
 }
 
-int vector_remove_ordered(Vector *vec, size_t index) 
-{
-	// out of bounds 
-	if (index >= vec->length) return -1; 	
 
-	void *empty_slot = vec->array + (index * vec->data_size);
-	
-	memmove(empty_slot, 
-			empty_slot + vec->data_size,
-			(vec->length - index + 1) * vec->data_size);
+int vector_remove_idx_ordered(Vector *vec, size_t index) 
+{
+	if (index >= vec->length) 
+		return -1; // out of bounds	
+
+	uint8_t *empty_slot = vec->array + (index * vec->data_size);
+	uint8_t *next_slot = empty_slot + vec->data_size;
+	size_t bytes_to_shift = (vec->length - index + 1) * vec->data_size;
+
+	memmove(empty_slot, next_slot, bytes_to_shift);
 
 	vec->length--;
 
-	if (vec->length <= vec->capacity / 2)
+	// check if vector needs to shrink
+	if (vec->length <= (vec->capacity / 2) && vec->capacity > vec->reserve)
 	{
 		vec->capacity /= 2;
 		vec->array = realloc(vec->array, (vec->capacity * vec->data_size));
 	}
 
 	return 0;
-
 }
+
 
 int vector_remove_idx_unordered(Vector *vec, size_t index) 
 {
-
 	if (index >= vec->length)
-	{
-		return -1; // out of bounds error or nothing in vector
-	}
+		return -1; // out of bounds 
 
-	void *empty_slot = vec->array + (index * vec->data_size);
-	void *last_slot = vec->array + ((vec->length - 1) * vec->data_size);
+	uint8_t *empty_slot = vec->array + (index * vec->data_size);
+	uint8_t *last_slot = vec->array + ((vec->length - 1) * vec->data_size);
 	
 	memcpy(empty_slot, last_slot, vec->data_size);
 
 	vec->length--;
 
-	if (vec->length <= vec->capacity / 2 && vec->capacity > vec->reserve)
+	// check if vector needs to shrink
+	if (vec->length <= (vec->capacity / 2) && vec->capacity > vec->reserve)
 	{
 		vec->capacity /= 2;
 		vec->array = realloc(vec->array, (vec->capacity * vec->data_size));
 	}
 
 	return 0;
+}
 
+int vector_remove_ptr_ordered(Vector *vec, void *item)
+{
+	uint8_t *base = vec->array;
+	uint8_t *empty_slot = item;
+
+	size_t total_bytes = vec->length * vec->data_size;
+
+	if(empty_slot < base || empty_slot >= (base + (total_bytes))) 
+		return -1; // out of bounds
+	
+	size_t offset = empty_slot - base;
+
+	if(offset % vec->data_size) 
+		return -1; // data not aligned
+	
+	size_t index = (offset / vec->data_size);
+				   
+	uint8_t *next_slot = empty_slot + vec->data_size;
+	size_t bytes_to_shift = (vec->length - index + 1) * vec->data_size;
+
+	memmove(empty_slot, next_slot, bytes_to_shift);
+
+	vec->length--;
+
+	// check if vector needs to shrink
+	if (vec->length <= (vec->capacity / 2) && vec->capacity > vec->reserve)
+	{
+		vec->capacity /= 2;
+		vec->array = realloc(vec->array, (vec->capacity * vec->data_size));
+	}
+
+	return 0;
 }
 
 int vector_remove_ptr_unordered(Vector *vec, void *item)
@@ -159,12 +197,13 @@ int vector_remove_ptr_unordered(Vector *vec, void *item)
 	if(offset % vec->data_size) 
 		return -1; // data not aligned
 
-	void *last_slot = vec->array + ((vec->length - 1) * vec->data_size);
+	uint8_t *last_slot = vec->array + ((vec->length - 1) * vec->data_size);
 	
 	memcpy(empty_slot, last_slot, vec->data_size);
 
 	vec->length--;
 
+	// check if vector needs to shrink
 	if (vec->length <= vec->capacity / 2 && vec->capacity > vec->reserve)
 	{
 		vec->capacity /= 2;
@@ -184,57 +223,10 @@ void vector_free(Vector *vec)
 	vec->data_size = 0;
 }
 
+
 void vector_clear(Vector *vec)
 {
 	vec->length = 0;
 	vec->capacity = vec->reserve;
 }
 
-typedef struct Person {
-	int id;
-	char *name;
-}Person;
-
-#define vector_foreach(T, item, vec)\
-	for(size_t _i_##item = 0; _i_##item < vec.length; _i_##item++)\
-		for(T *item = (T *)vector_unsafe_get_idx(&vec, _i_##item); item; item = NULL)
-
-ssize_t Tvector_indexof(Vector *vec, void *item)
-{
-	for(size_t i = 0; i < vec->length; i++) 
-	{
-		if (vec->array + (i * vec->data_size) == item) 
-		{
-			return i;
-		}
-	}
-	return -1;
-}
-
-int main() 
-{
-	Vector vector_people = vector_create(sizeof (Person), 1);
-
-	Person *person1 = vector_emplace_back(&vector_people);
-	person1->id = 324;
-	person1->name = "Bacon";
-
-	Person *person2 = vector_emplace_back(&vector_people);
-	if(person2) memset(person2, 0, sizeof (Person));
-
-	Person *person3 = vector_emplace_back(&vector_people);
-	if(person3){
-		person3->id = 100;
-		person3->name = "Satan";
-	}
-
-	// vector_remove_ptr_unordered(&vector_people, person2);
-
-	ssize_t index = vector_indexof(&vector_people, person1);
-	printf("The index of person3 is: %ld\n", index);
-
-	vector_foreach(Person, person, vector_people){
-		printf("index: %zu, value: %d, name: %s\n", _i_person, person->id, person->name);
-	}
-
-}
