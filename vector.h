de7444d9b41f7256047ecfb4725aeed9fbb03357
harrison-pc <harryscout@gmail.com>
2025-05-26 22:52:58 -0700

    doc(): add Doxygen stype documenation to vector.h Refactor vector_foreach to take pointer

--- vector.h
+++ vector.h
@@ -54,20 +54,124 @@ int vector_push_back(Vector *vec, const void *item);
 /**
  * @brief  Make room at end of the Vector for item.
  * @param  vec  Pointer to the Vector.
- * @return  Pointer to item slot, NULL on allocation failure
+ * @return  Pointer to item slot
+ * @return  NULL on allocation failure
+ * @example
+ * @code
+ * Person *p = (Person *)vector_emplace_back(vector_people);
+ * if (p) {
+ *		p->name = "Bob";
+ *		p->id = 1234;
+ * } else {
+ *		perror("malloc");
+ *		exit(EXIT_FAILURE);
+ * }
+ * @endcode
  */
 void *vector_emplace_back(Vector *vec) ;
+
+/**
+ * @brief  Get item from vector with index
+ * @param  vec    Pointer to the Vector.
+ * @param  index  Index of item to get.
+ * @return  Pointer to the item. 
+ * @return  NULL if out of bounds
+ */
 inline void* vector_get_idx(Vector *vec, size_t index);
+
+/**
+ * @brief  Private Function Used in foreach macro do not use this function. No safety checks.
+ * @param  vec    Pointer to the Vector.
+ * @param  index  Index of item to get.
+ * @return  Pointer to the item.
+ */
 static inline void* vector_unsafe_get_idx(Vector *vec, size_t index);
+
+/**
+ * @brief  Finds the index of matching pointers to an item in the Vector.
+ * @param  vec   Pointer to the Vector.
+ * @param  item  Pointer to the item.
+ * @return  Index on success.
+ * @return  -1 on out of bounds.
+ * @return  -2 on data miss-aligned.
+ */
 ssize_t vector_idx_of(Vector *vec, void *item) ;
+
+/**
+ * @brief  Removes item from the Vector while maintaining order of items. Slower than unordered.
+ * @details O(n) time complexity if not allocating
+ * @details O(n^2) if allocating.
+ * @param  vec    Pointer to the Vector.
+ * @param  index  Index of the item.
+ * @return  0 on success.
+ * @return  -1 on out of bounds.
+ */
 int vector_remove_idx_ordered(Vector *vec, size_t index) ;
+
+/**
+ * @brief  Removes item from the Vector with no regard to order of items. Faster than ordered.
+ * @details O(1) time complexity if not allocating
+ * @param  vec    Pointer to the Vector.
+ * @param  index  Index of the item.
+ * @return  0 on success.
+ * @return  -1 on out of bounds.
+ * @return  -2 on allocation failure
+ * @related vector_remove_idx_unordered
+ */
 int vector_remove_idx_unordered(Vector *vec, size_t index) ;
+
+/*
+ * @brief  Removes item from the Vector while maintaining order of items. Slower than unordered.
+ * @details O(n) time complexity if not allocating
+ * @details O(n^2) if allocating
+ * @param  vec   Pointer to the Vector.
+ * @param  item  Pointer to the Item.
+ * @return  0 on success.
+ * @return  -1 on out of bounds.
+ * @return  -2 on allocation failure.
+ * @returns -3 on data miss-aligned.
+ */
 int vector_remove_ptr_ordered(Vector *vec, void *item);
+
+/*
+ * @brief  Removes item from the Vector with no regard to the order of items. Faster than ordered.
+ * @details O(1) time complexity if not allocating
+ * @param  vec  Pointer to the Vector.
+ * @param  item  Pointer to the Item.
+ * @return  0 on success.
+ * @return  -1 on out of bounds.
+ * @return  -2 on allocation failure.
+ * @return  -3 on data miss-aligned.
+ */
 int vector_remove_ptr_unordered(Vector *vec, void *item);
+
+/*
+ * @brief removes all items from the Vector at once. 
+ * @details O(1) Time Complexity.
+ * @param  vec  Pointer to the Vector.
+ * @return  void
+ */
 void vector_clear(Vector *vec);
+
+/*
+ * @brief frees array and vector struct.
+ * @param  vec  Pointer to the Vector.
+ * @return void
+ */
 void vector_free(Vector *vec);
 
-#define vector_foreach(T, item, vec)\
-	for(size_t _i_##item = 0; _i_##item < vec.length; _i_##item++)\
-		for(T *item = (T *)vector_unsafe_get_idx(&vec, _i_##item); item; item = NULL)
+/*
+ * @brief  Macro that iterates over each item in Vector.
+ * @details use _i_##item for index. 
+ * @param  T  type of the item in the Vector.
+ * @param  item_name  name of item in the Vector.
+ * @param  p_vec  Pointer to the Vector.
+ * @example 
+ * vector_foreach(Person, person, vector_people) {
+ *		printf("Index: %zu, Name: %s, ID: %zu", _i_person, person->name, person->id);		
+ * }
+ */
+#define vector_foreach(T, item_name, p_vec)\
+	for(size_t _i_##item = 0; _i_##item < vec->length; _i_##item++)\
+		for(T *item = (T *)vector_unsafe_get_idx(vec, _i_##item); item; item = NULL)
 #endif
